f767819f8c2a54ce40b73dfed19d3d36306f9865
Samuel Courtier <court106@umn.edu>
2014-03-28 17:45:04 -0500

    Basic creature state advancement. Creatures grow steadily as a test.

--- creatures.js
+++ creatures.js
@@ -1,18 +1,19 @@
 (function(parent) {
+  var njs = numeric;
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var fps = 60;
   var t = 0;
   var dt = 1;
 
-  function buildSimState(numFood) {
+  function buildSimState(numFood, numCreatures) {
     function rndPos(rMax) {
       var rndRadius = Math.sqrt(Math.random()) * rMax;
       var rndAngle =  Math.random() * 2 * Math.PI;
       var rndx = rMax + rndRadius * Math.cos(rndAngle);
       var rndy = rMax + rndRadius * Math.sin(rndAngle);
       
-      return {x:rndx, y:rndy};
+      return [rndx, rndy];
     }
     
     var simState = {};
@@ -30,7 +31,7 @@
     }
 
     simState.creatureStates = [];
-    for (var csIdx = 0; csIdx < numFood; csIdx++) {
+    for (var csIdx = 0; csIdx < numCreatures; csIdx++) {
       var creatureState = {radius: 15,
                            pos: rndPos(simState.radius),
                            energy: 10
@@ -42,10 +43,20 @@
     return simState;
   }
 
+  function advance(simState, dt) {
+    function advanceCreature(creature) {
+      creature.radius = creature.radius * 1.001;
+    }
+
+    for (var csIdx = 0; csIdx < simState.creatureStates.length; csIdx++) {
+      advanceCreature(simState.creatureStates[csIdx]);
+    }
+  }
+
   function render(simState) {
     function renderFoodState(foodState) {
       ctx.beginPath();
-      ctx.arc(foodState.pos.x, foodState.pos.y, foodState.radius,
+      ctx.arc(foodState.pos[0], foodState.pos[1], foodState.radius,
               0, 2 * Math.PI);
       ctx.fillStyle = 'orange';
       ctx.strokeStyle = 'black';
@@ -54,8 +65,8 @@
     }
     function renderCreatureState(creatureState) {
       ctx.beginPath();
-      ctx.arc(creatureState.pos.x, creatureState.pos.y, creatureState.radius,
-              0, 2 * Math.PI);
+      ctx.arc(creatureState.pos[0], creatureState.pos[1],
+              creatureState.radius, 0, 2 * Math.PI);
       ctx.fillStyle = 'red';
       ctx.strokeStyle = 'black';
       ctx.fill();
@@ -64,20 +75,21 @@
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (var i = 0; i < simState.foodStates.length; i++) {
-      renderFoodState(simState.foodStates[i]);
+    for (var fsIdx = 0; fsIdx < simState.foodStates.length; fsIdx++) {
+      renderFoodState(simState.foodStates[fsIdx]);
     }
     
-    for (var i = 0; i < simState.creatureStates.length; i++) {
-      renderCreatureState(simState.creatureStates[i]);
+    for (var csIdx = 0; csIdx < simState.creatureStates.length; csIdx++) {
+      renderCreatureState(simState.creatureStates[csIdx]);
     }
   }
 
-  var simState = buildSimState(100);
+  var simState = buildSimState(100, 20);
 
   function loop() {
     t += dt;
 
+    advance(simState, dt);
     render(simState);
   }
   
